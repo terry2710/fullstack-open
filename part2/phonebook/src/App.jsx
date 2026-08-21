781fab195e5bcce0b08b3abf7a4960bfedd28b84
Tianren Wang <tianrenwang@Tianrens-Mac-mini.local>
2026-08-21 12:23:37 +1000

    part2c/2.11: fetch initial data from json-server for notes and phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,29 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123-456', id: 1},
-        {name: 'Ada Lovelace', number: '394-453-223', id: 2},
-        {name: 'Dan Abramov', number: '102-403-345', id: 3},
-        {name: 'Mary Poppendieck', number: '390-283-122', id: 4}
-    ])
+    // const [persons, setPersons] = useState([
+    //     {name: 'Arto Hellas', number: '040-123-456', id: 1},
+    //     {name: 'Ada Lovelace', number: '394-453-223', id: 2},
+    //     {name: 'Dan Abramov', number: '102-403-345', id: 3},
+    //     {name: 'Mary Poppendieck', number: '390-283-122', id: 4}
+    // ])
+
+    const [persons, setPersons] = useState([])
+    useEffect(() => {
+        console.log('effect')
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled')
+                setPersons(response.data)
+            })
+    }, [])
+    console.log('render', persons.length, 'persons')
+
     const [newName, setNewName] = useState('')
     const [newNum, setNewNum] = useState('')
     const [newFilter, setNewFilter] = useState('')
@@ -34,12 +48,12 @@ const App = () => {
     const addPerson = (event) => {
         event.preventDefault()
 
-        const numberPattern = /^\d{3}-\d{3}-\d{3}$/
-
-        if (!numberPattern.test(newNum)) {
-            alert('Number must use the format xxx-xxx-xxx')
-            return
-        }
+        // const numberPattern = /^\d{3}-\d{3}-\d{3}$/
+        //
+        // if (!numberPattern.test(newNum)) {
+        //     alert('Number must use the format xxx-xxx-xxx')
+        //     return
+        // }
 
         const nameExists = persons.some(
             person => person.name === newName
