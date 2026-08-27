5218f81aef7a8d37ff283baad97e06b90cb3cb83
TerryWang <tiaren.wang@uq.edu.au>
2026-08-27 16:39:31 +1000

    Serve phonebook frontend as static files from backend; remove unneeded cors dependency

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,10 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-const cors = require('cors')
-app.use(cors())
-
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
